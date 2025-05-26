1dbce21152c4281c61d6b289f39042916dcf2e1e
Shoko Kondo <ybuchi3168@gmail.com>
2025-05-27 01:19:56 +0900

    ヘッダ共通化

--- core/jinro_api.py
+++ core/jinro_api.py
@@ -5,11 +5,12 @@ class JinroAPI:
     BASE_URL = "http://toroneko.com/torojinro/api.cgi?"
 
     @staticmethod
+    def _headers():
+        return {"TOKEN": os.environ.get("JINRO_API_TOKEN")}
+
+    @staticmethod
     def get_vil(vid):
-        url = f"{JinroAPI.BASE_URL}cmd=vil&vid=" + vid
-        token = os.environ.get("JINRO_API_TOKEN")
-        headers = {"TOKEN": token}
-        response = requests.get(url, headers=headers)
+        url = f"{JinroAPI.BASE_URL}cmd=vil&vid={vid}"
+        response = requests.get(url, headers=JinroAPI._headers())
         response.raise_for_status()
         return response.json()
-
