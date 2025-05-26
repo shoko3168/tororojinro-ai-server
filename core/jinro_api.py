cb9adf3aa7ffb664c01011733919c47535ccc4ac
Shoko Kondo <ybuchi3168@gmail.com>
2025-05-27 03:53:48 +0900

    entry

--- core/jinro_api.py
+++ core/jinro_api.py
@@ -6,7 +6,10 @@ class JinroAPI:
 
     @staticmethod
     def _headers():
-        return {"TOKEN": os.environ.get("JINRO_API_TOKEN")}
+        return {
+            "TOKEN": os.environ.get("JINRO_API_TOKEN"),
+            "Content-Type": "application/x-www-form-urlencoded; charset=EUC-JP"
+        }
 
     @staticmethod
     def get_vil(vid):
@@ -14,3 +17,24 @@ class JinroAPI:
         response = requests.get(url, headers=JinroAPI._headers())
         response.raise_for_status()
         return response.json()
+
+    @staticmethod
+    def entry_vil(vid, agent):
+        # pid, message, skillは-1がおまかせ、-2がランダム, player_nameはfreeじゃなければ不要. authでuserid
+
+        # agent.first_message
+        url = f"{JinroAPI.BASE_URL}"
+        headers=JinroAPI._headers()
+        data = {
+            "cmd": "entry",
+            "userid": agent.userid,
+            "pid": agent.pid,
+            "message": agent.first_message.encode("euc-jp"),
+            "vid": vid
+        }
+        response = requests.post(url, headers=headers, data=data)
+        response.raise_for_status()
+        response.encoding = 'euc-jp'
+        print(response.text)
+        return response.text
+
--- test/test_jinro_api.py
+++ test/test_jinro_api.py
@@ -3,13 +3,21 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from core.jinro_api import JinroAPI
+from core.agent import Agent
 from dotenv import load_dotenv
 
 def test_get_vil():
     result = JinroAPI.get_vil(vid = "13")
     print(result)
 
+def test_entry_vil():
+    agent = Agent(userid="bot2", name="研究員 レイ", pid="7", personality="普通", role=None)
+    result = JinroAPI.entry_vil(vid="12", agent=agent)
+    print(result)
+
 if __name__ == "__main__":
     load_dotenv()
-    test_get_vil()
+    # test_get_vil()
+    test_entry_vil()
+    # JinroAPI.entry_test()
     print("JinroAPI.get_village_list test passed.")
