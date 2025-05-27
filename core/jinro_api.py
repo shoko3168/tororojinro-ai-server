2c7ef2d808889ea238fedf030fc069688ac366f9
Shoko Kondo <ybuchi3168@gmail.com>
2025-05-28 04:33:29 +0900

    JinroApiおおむね

--- core/jinro_api.py
+++ core/jinro_api.py
@@ -21,8 +21,6 @@ class JinroAPI:
     @staticmethod
     def entry_vil(vid, agent):
         # pid, message, skillは-1がおまかせ、-2がランダム, player_nameはfreeじゃなければ不要. authでuserid
-
-        # agent.first_message
         url = f"{JinroAPI.BASE_URL}"
         headers=JinroAPI._headers()
         data = {
@@ -35,6 +33,71 @@ class JinroAPI:
         response = requests.post(url, headers=headers, data=data)
         response.raise_for_status()
         response.encoding = 'euc-jp'
-        print(response.text)
         return response.text
 
+    @staticmethod
+    def msg(vid, player, message, opt=None):
+        url = f"{JinroAPI.BASE_URL}"
+        headers=JinroAPI._headers()
+        data = {
+            "cmd": "msg",
+            "userid": player.agent.userid,
+            "pid": player.agent.pid,
+            "message": message.encode("euc-jp"),
+            "vid": vid
+        }
+        if opt:
+            data.update(opt)
+        response = requests.post(url, headers=headers, data=data)
+        response.raise_for_status()
+        response.encoding = 'euc-jp'
+        return response.text
+
+    @staticmethod
+    def whisper(vid, player, message):
+        JinroAPI.msg(vid, player, message, opt={"whisper": "on"})
+
+    @staticmethod
+    def get_log(vid, date):
+        # type=thinkだと本人のみ見れる
+        # type=whisperhowlだと@player.can_whisperのみ見れる、そうでなければ「howl_wolf」
+        # type=whisperは@player.can_whisperがTrueの人のみ見れる
+        # type=groanは@player.deadが0の人のみ見れる
+        # type=fanaticは人狼告知（for信）
+        url = f"{JinroAPI.BASE_URL}cmd=log&vid={vid}&date={date}"
+        response = requests.get(url, headers=JinroAPI._headers())
+        response.raise_for_status()
+        return response.json()
+
+    @staticmethod
+    def vote(vid, player, set_date, target_id):
+        url = f"{JinroAPI.BASE_URL}"
+        headers = JinroAPI._headers()
+        data = {
+            "cmd": "vote",
+            "userid": player.agent.userid,
+            "vid": vid,
+            "set_date": set_date,
+            "vote_id": target_id
+        }
+        response = requests.post(url, headers=headers, data=data)
+        response.raise_for_status()
+        return response.text
+
+    @staticmethod
+    def skill(vid, player, set_date, target_id, target_id2=None):
+        # target_idはplayerのnum_idで設定
+        url = f"{JinroAPI.BASE_URL}"
+        headers = JinroAPI._headers()
+        data = {
+            "cmd": "skill",
+            "userid": player.agent.userid,
+            "vid": vid,
+            "set_date": set_date,
+            "target_id": target_id
+        }
+        if target_id2:
+            data["target_id2"] = target_id2
+        response = requests.post(url, headers=headers, data=data)
+        response.raise_for_status()
+        return response.text
--- test/test_jinro_api.py
+++ test/test_jinro_api.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from core.jinro_api import JinroAPI
 from core.agent import Agent
+from core.player import Player
 from dotenv import load_dotenv
 
 def test_get_vil():
@@ -11,13 +12,56 @@ def test_get_vil():
     print(result)
 
 def test_entry_vil():
-    agent = Agent(userid="bot2", name="研究員 レイ", pid="7", personality="普通", role=None)
+    agent = Agent(userid="bot2", name="研究員 レイ", pid="7", personality="普通", first_message="よろしくお願いします！")
     result = JinroAPI.entry_vil(vid="12", agent=agent)
     print(result)
+
+def test_get_log():
+    result = JinroAPI.get_log(vid="12", date="1")
+    print(result)
+
+def test_msg():
+    agent = Agent(userid="bot4", name="委員長 ナディア", pid="3", personality="普通")
+    player = Player(agent=agent, player_name="委員長 ナディア", role=None)
+    result = JinroAPI.msg(vid="12", player=player, message="こんにちは、みんな！")
+    print(result)
+
+def test_msg2():
+    agent = Agent(userid="bot1", name="詩人 アルベルト", pid="18", personality="普通")
+    player = Player(agent=agent, player_name=agent.name, role='狼')
+    result = JinroAPI.msg(vid="12", player=player, message="狼の独り言テスト")
+    print(result)
+
+def test_whisper():
+    agent = Agent(userid="bot1", name="詩人 アルベルト", pid="18", personality="普通")
+    player = Player(agent=agent, player_name=agent.name, role='狼')
+    JinroAPI.whisper(vid="12", player=player, message="これは狼のメッセージです。")
+
+def test_skill():
+    agent = Agent(userid="bot3", name="少女 アリス", pid="2", personality="普通")
+    player = Player(agent=agent, player_name=agent.name, role='占')
+    JinroAPI.skill(vid="12", player=player, set_date="2", target_id="9")
+
+def test_skill2():
+    agent = Agent(userid="bot1", name="詩人 アルベルト", pid="18", personality="普通")
+    player = Player(agent=agent, player_name=agent.name, role='狼')
+    JinroAPI.skill(vid="12", player=player, set_date="1", target_id="9")
+
+def test_vote():
+    agent = Agent(userid="bot3", name="少女 アリス", pid="2", personality="普通")
+    player = Player(agent=agent, player_name=agent.name, role='占')
+    JinroAPI.vote(vid="12", player=player, set_date="2", target_id="9")
 
 if __name__ == "__main__":
     load_dotenv()
     # test_get_vil()
-    test_entry_vil()
+    # test_entry_vil()
     # JinroAPI.entry_test()
-    print("JinroAPI.get_village_list test passed.")
+    # test_get_log()
+    # test_msg()
+    # test_whisper()
+    # test_msg2()
+    test_skill()
+    # test_skill2()
+    # test_vote()
+    print("JinroAPI tests passed.")
